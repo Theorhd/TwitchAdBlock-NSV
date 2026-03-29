8acaa36fd54bbb00b354272f800df0042681196c
Theo <theo.richard8654@gmail.com>
2026-03-29 07:51:24 +0200

    Enhance VOD manifest generation by adding bandwidth information for resolutions and updating manifest format

--- TWAdBlockVODUnlocker.m
+++ TWAdBlockVODUnlocker.m
@@ -123,15 +123,16 @@
     
     NSString *servingID = [[NSUUID UUID].UUIDString stringByReplacingOccurrencesOfString:@"-" withString:@""];
     NSMutableString *manifest = [NSMutableString stringWithFormat:@"#EXTM3U\n"
+                                 "#EXT-X-VERSION:3\n"
                                  "#EXT-X-TWITCH-INFO:ORIGIN=\"s3\",B=\"false\",REGION=\"EU\",USER-IP=\"127.0.0.1\",SERVING-ID=\"%@\",CLUSTER=\"cloudfront_vod\",USER-COUNTRY=\"BE\",MANIFEST-CLUSTER=\"cloudfront_vod\"\n", servingID];
     
     NSDictionary *resolutions = @{
-        @"160p30": @{@"res": @"284x160", @"fps": @30},
-        @"360p30": @{@"res": @"640x360", @"fps": @30},
-        @"480p30": @{@"res": @"854x480", @"fps": @30},
-        @"720p60": @{@"res": @"1280x720", @"fps": @60},
-        @"1080p60": @{@"res": @"1920x1080", @"fps": @60},
-        @"chunked": @{@"res": @"1920x1080", @"fps": @60}
+        @"160p30": @{@"res": @"284x160", @"fps": @30, @"bw": @638000},
+        @"360p30": @{@"res": @"640x360", @"fps": @30, @"bw": @1064000},
+        @"480p30": @{@"res": @"854x480", @"fps": @30, @"bw": @1562000},
+        @"720p60": @{@"res": @"1280x720", @"fps": @60, @"bw": @3708000},
+        @"1080p60": @{@"res": @"1920x1080", @"fps": @60, @"bw": @8254000},
+        @"chunked": @{@"res": @"1920x1080", @"fps": @60, @"bw": @8534000}
     };
     
     NSArray *keys = @[@"chunked", @"1080p60", @"720p60", @"480p30", @"360p30", @"160p30"];
@@ -140,8 +141,6 @@
     [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZ"];
     NSDate *createdDate = [dateFormatter dateFromString:createdAt];
     NSTimeInterval daysDiff = [[NSDate date] timeIntervalSinceDate:createdDate] / 86400.0;
-    
-    NSInteger startBandwidth = 8534030;
     
     for (NSString *resKey in keys) {
         NSString *streamUrl;
@@ -153,15 +152,13 @@
             streamUrl = [NSString stringWithFormat:@"twab://%@/%@/%@/index-dvr.m3u8", domain, vodSpecialID, resKey];
         }
 
-        
         NSString *quality = [resKey isEqualToString:@"chunked"] ? @"1080p" : resKey;
         NSString *enabled = [resKey isEqualToString:@"chunked"] ? @"YES" : @"NO";
+        NSDictionary *resInfo = resolutions[resKey];
         
         [manifest appendFormat:@"#EXT-X-MEDIA:TYPE=VIDEO,GROUP-ID=\"%@\",NAME=\"%@\",AUTOSELECT=%@,DEFAULT=%@\n", quality, quality, enabled, enabled];
-        [manifest appendFormat:@"#EXT-X-STREAM-INF:BANDWIDTH=%ld,CODECS=\"avc1.4D001E,mp4a.40.2\",RESOLUTION=%@,VIDEO=\"%@\",FRAME-RATE=%@\n", (long)startBandwidth, resolutions[resKey][@"res"], quality, resolutions[resKey][@"fps"]];
+        [manifest appendFormat:@"#EXT-X-STREAM-INF:BANDWIDTH=%@,CODECS=\"avc1.4D001E,mp4a.40.2\",RESOLUTION=%@,VIDEO=\"%@\",FRAME-RATE=%@\n", resInfo[@"bw"], resInfo[@"res"], quality, resInfo[@"fps"]];
         [manifest appendFormat:@"%@\n", streamUrl];
-        
-        startBandwidth -= 100;
     }
     
     return manifest;
